86d229d9598d1fa4782a5ac176b3acf81cb3c7b4
Polina Deniskina <polinaoioi67@gmail.com>
2023-05-04 18:38:29 +0300

    HomeWork4

--- src/main/java/lection_4_tree/RedBlackTree.java
+++ src/main/java/lection_4_tree/RedBlackTree.java
@@ -1,2 +1,83 @@
-package lection_4_tree;public class RedBlackTree {
+package lection_4_tree;
+
+import java.awt.*;
+
+public class RedBlackTree {
+    private class Node {
+        private int value;
+        private boolean color; //red -> true
+        private Node leftChild;
+        private Node rightChild;
+
+        Node(int value) {
+            this.value = value;
+            leftChild = null;
+            rightChild = null;
+            color = true;
+        }
+    }
+
+    Node rotateLeft(Node node) {
+        Node child = node.rightChild;
+        Node childLeft = child.leftChild;
+
+        child.leftChild = node;
+        node.rightChild = childLeft;
+
+        return child;
+    }
+
+    Node rotateRight(Node node) {
+       Node child = node.leftChild;
+       Node childRight = child.rightChild;
+
+       child.rightChild = node;
+       node.leftChild = childRight;
+
+       return child;
+    }
+
+    boolean isRed(Node node) {
+        if (node == null) {
+            return false;
+        }
+        return (node.color == true);
+    }
+
+    void swapColor(Node node1, Node node2) {
+        boolean temp = node1.color;
+        node1.color = node2.color;
+        node2.color = temp;
+    }
+
+    Node insert(Node node, int value) {
+        if (node == null) {
+            return new Node(value);
+        }
+        if (value < node.value) {
+            node.leftChild = insert(node.leftChild, value);
+        } else if (value > node.value) {
+            node.rightChild = insert(node.rightChild, value);
+        } else {
+            return node;
+        }
+
+        if (isRed(node.rightChild) && !isRed(node.leftChild)) {
+            node = rotateLeft(node);
+            swapColor(node, node.leftChild);
+        }
+
+        if (isRed(node.leftChild) && isRed(node.leftChild.leftChild)) {
+            node = rotateRight(node);
+            swapColor(node, node.rightChild);
+        }
+
+        if (isRed(node.leftChild) && isRed(node.rightChild)) {
+            node.color = !node.color;
+
+            node.leftChild.color = false;
+            node.rightChild.color = false;
+        }
+        return node;
+    }
 }
--- src/main/java/lection_4_tree/Tree.java
+++ src/main/java/lection_4_tree/Tree.java
@@ -1,2 +1,52 @@
-package lection_4_tree;public class Tree {
+package lection_4_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree {
+    Node root;
+    public class Node {
+        int value;
+        List<Node> children;
+    }
+
+    public  boolean exist(int value) {
+        if (root != null) {
+            Node node = find(root, value);
+            if (node != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Node find(Node node, int value) {
+        if (node.value == value) {
+            return node;
+        } else {
+            for (Node child : node.children) {
+                Node result = find(child, value);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    private Node find2(int value) {
+        List<Node> line = new ArrayList<>();
+        line.add(root);
+        while (line.size() > 0) {
+            List<Node> nextLine = new ArrayList<>();
+            for (Node node : line) {
+                if (node.value == value) {
+                    return node;
+                }
+                nextLine.addAll(node.children);
+            }
+            line = nextLine;
+        }
+        return null;
+    }
 }
