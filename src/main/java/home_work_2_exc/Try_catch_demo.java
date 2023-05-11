cf9e53ed0d28414b11a48ab5d16231bf625e9dcc
Polina Deniskina <polinaoioi67@gmail.com>
2023-05-11 19:36:33 +0300

    Home work 2

---
+++ src/main/java/home_work_2_exc/Try_catch_demo.java
@@ -0,0 +1,47 @@
+package home_work_2_exc;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Try_catch_demo {
+    public static void main(String[] args) {
+
+        /*
+            Задание 2
+        */
+        try {
+            int d = 0;
+            int[] intArray = {1, 6, 7, 5, 9, 5, 9};
+            double catchedRes1 =  intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+
+        // printUsersNumber();
+    }
+
+    /*
+        Реализуйте метод, который запрашивает у пользователя ввод
+        дробного числа (типа float), и возвращает введенное значение.
+        Ввод текста вместо числа не должно приводить к падению приложения,
+        вместо этого, необходимо повторно запросить у пользователя ввод данных.
+    */
+    public static void printUsersNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите дробное число");
+
+        try {
+            System.out.println(scanner.nextFloat());
+        } catch (InputMismatchException e) {
+            System.out.println("Неверный формат, дробная часть отделена ',' ");
+            printUsersNumber();
+        }
+    }
+
+
+
+}
